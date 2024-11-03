8bfdbd6859b6749f14b42423067635a26038e74e
Naila Rais <157850227+NailaRais@users.noreply.github.com>
2024-11-04 02:34:04 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import './index.css'; // Add your own CSS file if needed
 
 const container = document.getElementById('root');
 const root = createRoot(container); // Create a root.
