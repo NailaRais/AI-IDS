bc4b18af7e842006b30c63a15bce5fe4995bd5af
Naila Rais <157850227+NailaRais@users.noreply.github.com>
2024-11-04 02:10:43 +0530

    Update truffle-config.js

--- smart_contracts/truffle-config.js
+++ smart_contracts/truffle-config.js
@@ -1,18 +1,16 @@
 // Uncomment the following lines if you want to use environment variables
-// require('dotenv').config();
-// const { MNEMONIC, PROJECT_ID } = process.env;
+require('dotenv').config();
+const { MNEMONIC, PROJECT_ID } = process.env;
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*" // Connects to any network
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Ganache port (default: 8545)
+      network_id: "*",       // Any network (default: none)
     },
-    // Uncomment and configure the following for Goerli network if needed
-    /*
     goerli: {
       provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${PROJECT_ID}`),
       network_id: 5,       // Goerli's id
@@ -20,7 +18,6 @@ module.exports = {
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
-    */
   },
 
   compilers: {
@@ -45,3 +42,4 @@ module.exports = {
   //   }
   // }
 };
+
