8bf31c3b8b7af54208f48ca7a5f62626860878af
Naila Rais <157850227+NailaRais@users.noreply.github.com>
2024-11-04 00:28:25 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
-import Web3Provider from './Web3Provider';
+import { Web3Provider } from './Web3Provider'; // Updated to use named import
 
 const App = () => {
     return (
